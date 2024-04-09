85d36bf4182220f00c3517f8a137393741834b47
laporeon <34722707+laporeon@users.noreply.github.com>
2024-04-09 15:26:59 -0300

    Delete symlinks.sh

--- symlinks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/.config/nvim/
-ln -sf $PWD/nvim ~/.config/nvim/
-
-ln -sf $PWD/zsh/.zshrc ~/.zshrc
-
-touch ~/.oh-my-zsh/themes/dcf.zsh-theme
-ln -sf $PWD/themes/dcf.zsh-theme ~/.oh-my-zsh/themes/dcf.zsh-theme
