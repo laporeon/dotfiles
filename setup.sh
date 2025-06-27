fd68c64c480a533eee62feac607421a2cdf6e0ce
laporeon <175569928+laporeon@users.noreply.github.com>
2025-06-27 15:12:35 -0300

    feat: add command to copy conventional commits guide to local folder

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 BLUE="\033[1;34m"
 RESET='\033[0m'
 
+DEVELOPMENT="$HOME/Development"
 DOTFILES="$HOME/Development/dotfiles"
 
 echo -e "\n${BLUE}Installing JetBrains Mono NerdFont...${RESET}"
@@ -13,5 +14,8 @@ echo -e "\n${BLUE}Copying nvim, zsh and custom theme configuration...${RESET}"
 cp -r $DOTFILES/environments/linux/nvim $HOME/.config/nvim/
 cp $DOTFILES/environments/linux/zsh/.zshrc $HOME/
 cp $DOTFILES/environments/linux/zsh/themes/dcf.zsh-theme $HOME/.oh-my-zsh/themes/
+
+echo -e "\n${BLUE}Copying conventional commits guide...${RESET}"
+cp $DOTFILES/helpers/conventional-commits.sh $DEVELOPMENT
 
 echo -e "\n${BLUE}Setup completed!${RESET}"
