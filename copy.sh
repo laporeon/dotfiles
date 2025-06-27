d5184338b0b426d05287a1aa3d72da63838c0ca1
laporeon <175569928+laporeon@users.noreply.github.com>
2025-06-27 14:56:10 -0300

    feat: rename script, add fonts installation commands and copying files

--- copy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp -r $PWD/linux/nvim ~/.config/nvim/
-
-cp -r $PWD/linux/zsh/.zshrc ~/.zshrc
-
-cp -r $PWD/linux/zsh/themes/dcf.zsh-theme ~/.oh-my-zsh/themes/dcf.zsh-theme
---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BLUE="\033[1;34m"
+RESET='\033[0m'
+
+DOTFILES="$HOME/Development/dotfiles"
+
+echo -e "\n${BLUE}Installing JetBrains Mono NerdFont...${RESET}"
+sudo cp -r $DOTFILES/assets/fonts/JetBrainsMonoNerdFont/*.ttf /usr/share/fonts/
+sudo fc-cache -fv
+
+echo -e "\n${BLUE}Copying nvim, zsh and custom theme configuration...${RESET}"
+cp -r $DOTFILES/environments/linux/nvim $HOME/.config/nvim/
+cp $DOTFILES/environments/linux/zsh/.zshrc $HOME/
+cp $DOTFILES/environments/linux/zsh/themes/dcf.zsh-theme $HOME/.oh-my-zsh/themes/
+
+echo -e "\n${BLUE}Setup completed!${RESET}"
