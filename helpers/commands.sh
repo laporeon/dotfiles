d41a1b0caafe666c897ef72b4173978f629f0eb6
laporeon <175569928+laporeon@users.noreply.github.com>
2026-03-04 16:20:09 -0300

    fix: fix command to create local folders and adjust some colors and spacing

--- helpers/commands.sh
+++ helpers/commands.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 GREEN='\033[0;32m'
-BLUE="\033[1;34m"
+BOLD_WHITE="\033[1;34m"
 RED='\033[0;31m'
 RESET='\033[0m'
+BOLD_WHITE='\033[1;37m'
 
 DOCUMENTS="$HOME/Documents"
 DEVELOPMENT="$HOME/Development"
@@ -15,84 +16,84 @@ error() {
 
 # ===== INSTALLATIONS / UPDATE ===== #
 install_jetbrains_font() {
-    echo -e "\n${BLUE}Installing JetBrains Mono NerdFont...${RESET}"
+    echo -e "${BOLD_WHITE}Installing JetBrains Mono NerdFont...${RESET}"
     sudo cp -r "$DOTFILES/assets/fonts/JetBrainsMonoNerdFont/"*.ttf /usr/share/fonts/ && \
     sudo fc-cache -fv && \
-    echo -e "${GREEN}\n✔ JetBrains Mono NerdFont successfully installed!${RESET}" || error
+    echo -e "${GREEN}✔ JetBrains Mono NerdFont successfully installed!${RESET}" || error
 }
 
 update_discord() {
-    echo -e "\n${BLUE}Updating Discord...${RESET}"
+    echo -e "${BOLD_WHITE}Updating Discord...${RESET}"
     curl -sL "https://discord.com/api/download/stable?platform=linux&format=deb" -o /tmp/discord.deb && \
     sudo dpkg -i /tmp/discord.deb && \
     rm /tmp/discord.deb && \
-    echo -e "${GREEN}\n✔ Discord successfully updated!${RESET}" || error
+    echo -e "${GREEN}✔ Discord successfully updated!${RESET}" || error
 }
 
 update_bruno() {
-    echo -e "\n${BLUE}Updating Bruno...${RESET}"
+    echo -e "${BOLD_WHITE}Updating Bruno...${RESET}"
     local bruno_url=$(curl -s https://api.github.com/repos/usebruno/bruno/releases/latest | grep -o 'https://[^"]*_amd64_linux\.deb' | head -n1)
     curl -sL "$bruno_url" -o /tmp/bruno.deb && \
     sudo dpkg -i /tmp/bruno.deb && \
     rm /tmp/bruno.deb && \
-    echo -e "${GREEN}\n✔ Bruno successfully updated!${RESET}" || error
+    echo -e "${GREEN}✔ Bruno successfully updated!${RESET}" || error
 }
 
 update_dbeaver() {
-    echo -e "\n${BLUE}Updating DBeaver...${RESET}"
+    echo -e "${BOLD_WHITE}Updating DBeaver...${RESET}"
     curl -sL "https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb" -o /tmp/dbeaver.deb && \
     sudo dpkg -i /tmp/dbeaver.deb && \
     rm /tmp/dbeaver.deb && \
-    echo -e "${GREEN}\n✔ DBeaver successfully updated!${RESET}" || error
+    echo -e "${GREEN}✔ DBeaver successfully updated!${RESET}" || error
 }
 
 # =========== BACKUPS AND SYNCHRONIZATIONS ========== #
 
 lsync() {
-  echo -e "\n${BLUE}Syncing config files...${RESET}"
+  echo -e "${BOLD_WHITE}Syncing config files...${RESET}"
 
-  mkdir -p "$HOME/.config/{nvim,wezterm,fastfetch}"
+  mkdir -p "$HOME"/.config/{nvim,wezterm,fastfetch}
 
   cp -r "$DOTFILES/environments/linux/nvim" "$HOME/.config/" && \
   cp -r "$DOTFILES/environments/linux/wezterm" "$HOME/.config/" && \
   cp -r "$DOTFILES/environments/linux/fastfetch" "$HOME/.config/" && \
   cp "$DOTFILES/environments/linux/zsh/.zshrc" "$HOME/" && \
-  cp "$DOTFILES/environments/linux/zsh/themes/dcf.zsh-theme" "$HOME/.oh-my-zsh/themes/" && \
-  echo -e "${GREEN}\n✔ Configuration files successfully synced!${RESET}" || error
+  cp "$DOTFILES/environments/linux/zsh/themes/"*.zsh-theme "$HOME/.oh-my-zsh/themes/" && \
+  echo -e "${GREEN}✔ Configuration files successfully synced!${RESET}" || error
 }
 
 dsync() {
-  echo -e "\n${BLUE}↑ Syncing documents with Dropbox...${RESET}"
+  echo -e "${BOLD_WHITE}↑ Syncing documents with Dropbox...${RESET}"
 
   rclone sync "$DOCUMENTS/Obsidian" dropbox:Obsidian && \
   rclone sync "$DOCUMENTS/Async" dropbox:Async && \
   rclone sync "$DOCUMENTS/Currículos" dropbox:Curriculos && \
-  echo -e "${GREEN}\n✔ Documents successfully synced!${RESET}" || error
+  echo -e "${GREEN}✔ Documents successfully synced!${RESET}" || error
 }
 
 dtfcp() {
-  echo -e "\n${BLUE}Backing up dotfiles...${RESET}"
+  echo -e "${BOLD_WHITE}Backing up dotfiles...${RESET}"
 
   cp "$HOME/.zshrc" "$DOTFILES/environments/linux/zsh/" && \
   cp -r "$HOME/.config/nvim/." "$DOTFILES/environments/linux/nvim/" && \
   cp "$HOME/.config/wezterm/wezterm.lua" "$DOTFILES/environments/linux/wezterm/" && \
   cp "$HOME/.config/fastfetch/config.jsonc" "$DOTFILES/environments/linux/fastfetch/" && \
-  echo -e "${GREEN}\n✔ Dotfiles successfully backed up!${RESET}" || error
+  echo -e "${GREEN}✔ Dotfiles successfully backed up!${RESET}" || error
 }
 
 bkp() {
-  echo -e "\n${BLUE}Starting backup process...${RESET}"
+  echo -e "${BOLD_WHITE}Starting backup process...${RESET}"
 
   # Development folder
-  echo -e "\n${BLUE}Backing up Development folder...${RESET}"
+  echo -e "${BOLD_WHITE}Backing up Development folder...${RESET}"
   zip -qr /tmp/development.zip "$DEVELOPMENT" && \
-  echo -e "${BLUE}↑ Syncing Development folder with Dropbox...${RESET}" && \
+  echo -e "${BOLD_WHITE}↑ Syncing Development folder with Dropbox...${RESET}" && \
   rclone sync /tmp/development.zip dropbox:Development && \
   rm /tmp/development.zip
 
   # Full backup
   local file="backup_$(date +%Y-%m-%d_%H-%M).zip"
-  echo -e "${BLUE}Creating $file...${RESET}"
+  echo -e "${BOLD_WHITE}Creating $file...${RESET}"
 
   zip -qr "$HOME/$file" \
       "$HOME/.themes" \
@@ -104,10 +105,10 @@ bkp() {
       "$HOME/Music" \
       "$HOME/Videos"
 
-  echo -e "${BLUE}Moving backup to external drive...${RESET}"
+  echo -e "${BOLD_WHITE}Moving backup to external drive...${RESET}"
   mv "$HOME/$file" /mnt/sda1/ && \
   local backup_size=$(du -h /mnt/sda1/"$file" | cut -f1) && \
-  echo -e "${GREEN}\n✔ Backup completed: $file (${backup_size})${RESET}" || error
+  echo -e "${GREEN}✔ Backup completed: $file (${backup_size})${RESET}" || error
 }
 
 # Backup script of Clipboard Indicator favorites
@@ -118,7 +119,7 @@ clipboard_backup() {
   BACKUP_FILE="$BACKUP_DIR/clipboard-favorites-$(date +%Y%m%d-%H%M%S).txt"
   LATEST_BACKUP="$BACKUP_DIR/clipboard-favorites-LATEST.txt"
 
-  echo -e "\n${BLUE}Backing up Clipboard favorites (Date: $(date)) ${RESET}"
+  echo -e "${BOLD_WHITE}Backing up Clipboard favorites (Date: $(date)) ${RESET}"
 
   if ! command -v jq >/dev/null; then
       echo "ERROR: Install jq to process multiline favorites."
@@ -130,7 +131,7 @@ clipboard_backup() {
       exit 1
   fi
 
-  echo "\nExtracting favorites..."
+  echo "Extracting favorites..."
 
   FAVORITES=$(jq -r '.[] | select(.favorite == true) | .contents' "$REGISTRY_FILE" 2>/dev/null)
 
@@ -160,10 +161,10 @@ clipboard_backup() {
       fi
   done
 
-  echo "\nTimestamped backup file saved to: $BACKUP_FILE"
+  echo "Timestamped backup file saved to: $BACKUP_FILE"
   echo "Latest backup file updated: $LATEST_BACKUP"
 
   ls -t "$BACKUP_DIR"/clipboard-favorites-*.txt | tail -n +3 | xargs rm -f 2>/dev/null
 
-  echo -e "${GREEN}\n✔ Backup successfully finished!${RESET}" || error
+  echo -e "${GREEN}✔ Backup successfully finished!${RESET}" || error
 }
